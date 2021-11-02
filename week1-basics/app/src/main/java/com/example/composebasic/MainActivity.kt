522004297d611796d5d4f9fd2ee912e4e781774d
kdh <daeho@wantedlab.com>
2021-11-02 16:47:09 +0900

    [week1] Finishing touches!

--- week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
+++ week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -19,8 +20,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,47 +60,52 @@ private fun Greetings(names: List<String> = List(1000) { "$it"} ) {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
 
+@Composable
+fun CardContent(name: String) {
     var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(
-        if(expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp,horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
-            IconButton(onClick = { expanded = !expanded }) {
-                Icon(
-                    imageVector =  if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription = if(expanded)  {
-                        stringResource(R.string.show_less)
-                    }
-                    else {
-                        stringResource(R.string.show_more)
-                    }
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
-            }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(if(expanded) "show less" else "Show More")
-
+            )
+    ){
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name,fontWeight = FontWeight.Bold, fontSize = 30.sp)
+            if(expanded) {
+                Text( text = ("Basic Compose samples\n").repeat(4))
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector =  if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if(expanded)  {
+                    stringResource(R.string.show_less)
+                }
+                else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
+
+}
+@Composable
+fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp,horizontal = 8.dp)
+    ) {
+        CardContent(name)
     }
 
 }
@@ -135,5 +143,14 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 fun DefaultPreview() {
     ComposeBasicTheme {
         Greetings()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
