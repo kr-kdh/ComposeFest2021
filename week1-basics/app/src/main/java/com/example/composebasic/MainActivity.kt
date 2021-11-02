1fc832a537c171717ccffce3ecf2d02a2500b67d
kdh <daeho@wantedlab.com>
2021-11-02 16:01:01 +0900

    (week1)  Animating your list

--- week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
+++ week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -52,10 +55,15 @@ private fun Greetings(names: List<String> = List(1000) { "$it"} ) {
 @Composable
 fun Greeting(name: String) {
 
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if(expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -69,8 +77,8 @@ fun Greeting(name: String) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if(expanded.value) "show less" else "Show More")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if(expanded) "show less" else "Show More")
 
             }
         }
