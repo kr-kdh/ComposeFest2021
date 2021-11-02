c1c60595c08d6474da470938283d815ed268970b
kdh <daeho@wantedlab.com>
2021-11-02 16:20:40 +0900

    (week1)  coerceAtLeast 적용안되서 죽는 문제수정, IconButton 적용

--- week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
+++ week1-basics/app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebasic.ui.theme.ComposeBasicTheme
@@ -73,10 +77,21 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
+            }
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector =  if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if(expanded)  {
+                        stringResource(R.string.show_less)
+                    }
+                    else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
             OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(if(expanded) "show less" else "Show More")
