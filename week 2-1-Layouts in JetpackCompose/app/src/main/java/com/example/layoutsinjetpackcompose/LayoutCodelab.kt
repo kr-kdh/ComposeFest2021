820b9683007e1b1551f056cc64cb9d2c8288d934
kdh <daeho@wantedlab.com>
2021-11-12 13:17:55 +0900

    [week2-1] Material Components

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutsinjetpackcompose/LayoutCodelab.kt
@@ -0,0 +1,48 @@
+package com.example.layoutsinjetpackcompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite , contentDescription = null)
+
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutCodeLabPreview() {
+    LayoutsInJetpackComposeTheme {
+        LayoutsCodelab()
+    }
+}
--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutsinjetpackcompose/PhotographerCard.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutsinjetpackcompose/PhotographerCard.kt
@@ -1,8 +1,5 @@
 package com.example.layoutsinjetpackcompose
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -22,24 +19,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            LayoutsInJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    LayoutCodeLabPreview()
-                }
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /* Ignoring onClick */ })
+        .padding(16.dp)
+    )  {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
             }
         }
     }
 }
 
-
 @Preview
 @Composable
-fun DefaultPreview() {
+fun PhotographerPreview() {
     LayoutsInJetpackComposeTheme {
         PhotographerCard()
     }
